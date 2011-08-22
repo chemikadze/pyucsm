cf29599d2ab5b6845d89d70c8feb8a4375ef8bfc
Nikolay Sokolov <nsokolov@griddynamics.com>
2011-08-22 16:51:05 +0400

    ucsmquery rewritten, pep8

--- ucsmquery.py
+++ ucsmquery.py
@@ -1,7 +1,54 @@
 #!/usr/bin/python
+
 from pyucsm import *
 import getopt
+from inspect import getargspec
 import sys
+
+
+IGNORE = []
+CONN_CLS = UcsmConnection
+ONLY_DN = False
+HIERARCHY = False
+
+
+def gener_descr(func):
+    args = getargspec(func)[0]
+    args = args[args[0] == 'self':]
+    templ = "Method:\t\t%(name)s"
+    if args:
+        templ += '\nArguments:\t%(args)s'
+    if func.func_doc:
+        templ += '\nDocumentation:\t%(doc)s'
+    return templ % {'name': func.func_name, 'args': ', '.join(args),
+       'doc': func.func_doc}
+
+
+def get_possible_opts(cls):
+    return list(set('%s=' % arg
+            for attr_name, attr in cls.__dict__.items()
+                if attr_name not in IGNORE \
+                    and attr_name[0] != '_' \
+                    and hasattr(attr, 'func_name')
+                    for arg in getargspec(attr)[0][1:]))
+
+
+def create_doc(cls):
+    descr = '\n\n'.join(gener_descr(attr)
+            for attr_name, attr in cls.__dict__.items()
+                if attr_name not in IGNORE \
+                    and attr_name[0] != '_' \
+                    and hasattr(attr, 'func_name'))
+    return descr
+
+
+def print_help(func_name):
+    cls = CONN_CLS
+    try:
+        print gener_descr(getattr(cls, func_name))
+    except AttributeError:
+        return usage()
+
 
 def usage():
     print """Usage: ucsmquery.py host[:port] [options] command [arguments].
@@ -12,13 +59,18 @@ Options:
 
 Commands:
 
+%s
+
 Arguments for UCSM query must be used as long options. Sample:
 
-ucsmquery.py example.com -l admin -p admin configFindDnsByClassId --classId=computeItem
-"""
+ucsmquery.py example.com -l admin -p admin configFindDnsByClassId\
+ --classId=computeItem
+""" % create_doc(UcsmConnection)
+
 
 def wrong_command(command=None):
     print """Command not found or incorrect arguments."""
+
 
 def print_objects(objects, only_dn=False, hierarchy=False):
     if only_dn:
@@ -42,77 +94,114 @@ def print_objects(objects, only_dn=False, hierarchy=False):
                 print_objects(obj.children, only_dn, hierarchy)
             newline = True
 
+
+def print_objects_glob(objects):
+    print_objects(object, ONLY_DN, HIERARCHY)
+
+
+def serialize_print(data):
+    if isinstance(data, list):
+        if len(data) and isinstance(data[0], UcsmObject):
+            print_objects(data)
+        else:
+            for elem in data:
+                serialize_print(elem)
+    if isinstance(data, UcsmObject):
+        print_objects([data])
+    if isinstance(data, dict):
+        for key, val in data.items():
+            print '%s:'
+            serialize_print(val)
+    if isinstance(data, basestring):
+        print data
+
+
+def kwargs_to_ucsm_object(cls_, **kwargs):
+    obj = UcsmObject(cls_)
+    for key, val in kwargs:
+        setattr(obj, key, val)
+    return obj
+
+
+def parse_opt_val(string):
+    try:
+        if string.startswith('{') or \
+            string.startswith('obj('):
+            raise Exception
+        return eval(string, {'obj': kwargs_to_ucsm_object,
+                             'UcsmObject': UcsmObject})
+    except Exception:
+        return string
+
+
+def kwargs_from_opts(opts):
+    kwargs = {}
+    for opt, val in opts.items():
+        kwargs[opt] = parse_opt_val(val)
+    return kwargs
+
+
 def perform(host, login, password, command, args=list(), opts=dict(), port=80):
-    client = UcsmConnection(host, port)
+    client = CONN_CLS(host, port)
     try:
         global quiet
         client.login(login, password)
-        if command == 'configFindDnsByClassId':
-            class_id = opts.get('classId')
-            dns = client.find_dns_by_class_id(class_id)
-            for dn in dns:
-                print dn
-        elif command == 'configResolveDn':
-            dn = opts.get('dn')
-            obj = client.resolve_dn(dn)
-            if obj:
-                print obj.pretty_str()
-            else:
-                print 'Object not found'
-        elif command == 'configResolveDns':
-            hierarchy = 'inHierarchical' in opts
-            objects,unresolved = client.resolve_dns(args, hierarchy)
-            print_objects(objects, only_dn=quiet, hierarchy=hierarchy)
-        elif command == 'configResolveChildren':
-            class_id = opts.get('classId', '')
-            dn = opts.get('inDn', '')
-            hierarchy = 'inHierarchical' in opts
-            objects = client.resolve_children(class_id, dn, hierarchy)
-            print_objects(objects, only_dn=quiet, hierarchy=hierarchy)
-        elif command == 'configResolveClasses':
-            hierarchy = 'inHierarchical' in opts
-            objects = client.resolve_classes(args, hierarchy)
-            print_objects(objects, only_dn=quiet, hierarchy=hierarchy)
-        else:
-            raise KeyError
-    except KeyError:
+        reply = getattr(client, command)(*args, **kwargs_from_opts(opts))
+        print serialize_print(reply)
+    except (KeyError, AttributeError):
         wrong_command()
     except UcsmError, e:
         print "Error: %s" % e
     finally:
         client.logout()
 
+
+def import_class(path):
+    mod, cls = path.rsplit('.', 1)
+    return getattr(__import__(mod, fromlist=[cls]), cls)
+
+
 if __name__ == '__main__':
     try:
         argv = sys.argv[1:]
-        opts, args = getopt.gnu_getopt(argv, 'l:p:P:dq', ["classId=", "dn=", "inDn=", "inHierarchical"])
-    except getopt.GetoptError:
+        opts, args = getopt.gnu_getopt(argv, 'l:p:P:dqc',
+                                       get_possible_opts(CONN_CLS))
+    except getopt.GetoptError, e:
         usage()
+        print e
         exit()
     login = 'admin'
     password = 'nbv12345'
     comm_opts = {}
     global quiet
     quiet = False
-    for opt,val in opts:
-        if opt=='-l':
+    for opt, val in opts:
+        if opt == '-l':
             login = val
-        elif opt=='-p':
+        elif opt == '-p':
             password = val
-        elif opt=='-d':
+        elif opt == '-d':
             import pyucsm
-            pyucsm.DEBUG = True
-        elif opt[:2]=='--':
+            pyucsm.set_debug(True)
+        elif opt[:2] == '--':
             comm_opts[opt[2:]] = val
-        elif opt=='-q':
-            quiet = True
-    if len(args)>=2:
+        elif opt == '-q':
+            ONLY_DN = True
+        elif opt == '-r':
+            HIERARCHY = True
+        elif opt == '-c':
+            CONN_CLS = import_class(val)
+    if len(args) >= 2:
+        if args[0] == 'help':
+            print_help(args[1])
+            exit(0)
         port = 80
         host = args[0]
         colon = args[0].find(':')
-        if colon>=0:
-            host = args[:colon]
-            port = int(args[colon+1:])
-        perform(args[0], login, password, args[1], args=args[2:], opts=comm_opts, port=port)
+        if colon >= 0:
+            host = args[0][:colon]
+            port = int(args[0][colon + 1:])
+        perform(host, login, password, args[1], args=args[2:],
+                opts=comm_opts, port=port)
     else:
         usage()
