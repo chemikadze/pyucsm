5ad3f5caa6928215189a5044e656fac79a814c1b
Nikolay Sokolov <nsokolov@griddynamics.com>
2011-09-26 14:21:16 +0400

    Removed explicit address of reference UCS system

--- tests/reference_system.py
+++ tests/reference_system.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-HOST = '172.16.45.128'
+HOST = 'ucsm-vm'
 LOGIN = 'admin'
 PASSWORD = 'nbv12345'
 SIMULATOR = True # some test affects to data, those tests are recommended to perform only on UCSPE  and will be ran only if this flag is True
